5fc442d3dbc463f2e0589bd39efa93fbd6913cff
kaw393939 <keith@webizly.com>
2014-10-03 11:21:22 -0400

    added boilerplate

--- index.php
+++ index.php
@@ -1,31 +1,90 @@
 <?php
-//this is how you instantiate an object 
-  $obj = new another;
-//this is how you assign a value to a public property
-  $obj->name = 'steve';
-//this is how you call a public method
-  $obj->printHelloWorld();
-//this is how you print the contents of an array or object
-  print_r($obj);
-//this is how you define a class
-  class example {
-//this is how you define a public property    
-    public $name;
 
-//this is the magic method constructor
-    public function __construct() {
-      echo 'Hello world... ' . $this->name;
-    }
-//this is creating a public method
-    public function printHelloWorld() {
-      echo 'Hello world... ' . $this->name . "\n";
-    }
-    protected function someFunction() {}
+$page_type = $_GET['pagetype'];
 
-    private function someOtherFunction() {}
+$obj = new $page_type;
+
+class page {
+
+   public $content;
+   
+   function __construct() {
+      $this->content = '<!doctype html>
+      <html class="no-js" lang="">
+          <head>
+	          <meta charset="utf-8">
+		          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+			          <title></title>
+				          <meta name="description" content="">
+					          <meta name="viewport"
+						  content="width=device-width,
+						  initial-scale=1">
+						  
+						          <!-- Place favicon.ico
+							       and
+							       apple-touch-icon(s)
+							       in the root
+							       directory -->
+							       
+							               <link
+								       rel="stylesheet"
+								       href="css/normalize.css">
+								               <link
+									       rel="stylesheet"
+									       href="css/main.css">
+									               <script
+										       src="js/vendor/modernizr-2.8.3.min.js"></script>
+										           </head>
+											       <body>';
+
    }
 
-   class another extends example {}
+   function __destruct() {
+     $this->addContent();
+     $this->content .= '
+     <script
+     src="//ajax.googleapis.com/ajax/libs/jquery/{{JQUERY_VERSION}}/jquery.min.js"></script>
+             <script>window.jQuery || document.write(\'<script
+	     src="js/vendor/jquery-{{JQUERY_VERSION}}.min.js"><\/script>\')</script>
+	             <script src="js/plugins.js"></script>
+		             <script src="js/main.js"></script>
+
+			             <!-- Google Analytics: change UA-XXXXX-X to
+					  be your site\'s ID. -->
+					          <script>
+						              (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+							                  function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new
+									  Date;
+									              e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+										                  e.src=\'//www.google-analytics.com/analytics.js\';
+												              r.parentNode.insertBefore(e,r)}(window,document,\'script\',\'ga\'));
+													                  ga(\'create\',\'UA-XXXXX-X\',\'auto\');ga(\'send\',\'pageview\');
+															          </script>
+																      </body>
+																      </html>';
+
+
+     echo $this->content;
+   }
+}
+
+class index extends page {
+
+  
+  function addContent() {
+
+    $this->content .= 'i\'m an index page';
+  }
+  
+
+}
+
+
+
+
+
+
+
 
 
 
